c90c1400ec1ff5849926d52087a6ae9e83008eaa
Campbelle-mac <campbelle@macstudent.com.au>
2026-06-11 08:54:53 +1000

    Created Cave class and constructor

---
+++ cave.py
@@ -0,0 +1,10 @@
+class Cave:
+    def __init__(self, cave_name, cave_size, cave_description, cave_items, cave_character):
+        self.name = cave_name
+        self.size = cave_size
+        self.description = cave_description
+        self.items = cave_items
+        self.characer = cave_character
+
+'''cave1 = Cave("grotto", "big", "dusty and dark", "rock", "spider")
+print(cave1.name)'''
