f956f223cb916192cf52e76c4414d1b8c2004db3
Campbelle-mac <campbelle@macstudent.com.au>
2026-06-17 11:16:50 +1000

    Progress on cave.py & main.py.

--- cave.py
+++ cave.py
@@ -5,6 +5,29 @@ class Cave:
         self.description = cave_description
         self.items = cave_items
         self.characer = cave_character
+        self.linked_caves = {}
 
-'''cave1 = Cave("grotto", "big", "dusty and dark", "rock", "spider")
-print(cave1.name)'''
+    def set_description(self, cave_description):
+        self.description = cave_description
+    
+    def get_description(self):
+        return self.description
+    
+    def describe(self):
+        print(f"The {self.get_name()}.")
+        print(self.description)
+
+    def link_cave(self, cave_to_link, direction):
+        self.linked_caves[direction] = cave_to_link
+
+    def get_name(self):
+        return self.name
+
+    def get_details(self):
+        for direction in self.linked_caves:
+            cave = self.linked_caves[direction]
+            print( "The " + cave.get_name() + " is " + direction + ".")
+
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from cave import Cave
 
+#Cavern Object
 cavern = Cave("cavern", None, None, None, None)
-grotto = Cave("grotto", None, None, None, None)
+cavern.set_description("A dank and dirty cave.")
+cavern.get_description()
+cavern.describe()
+
+#Dungeon Object
 dungeon = Cave("dungeon", None, None, None, None)
+dungeon.set_description("A large cave with a rack")
+
+#Grotto Object
+grotto = Cave("grotto", None, None, None, None)
+grotto.set_description("A small cave with ancient graffiti")
+
+#Cave Links
+cavern.link_cave(dungeon, "south")
+grotto.link_cave(dungeon, "east")
+dungeon.link_cave(grotto, "west")
+
+
+print(cavern.get_details())
